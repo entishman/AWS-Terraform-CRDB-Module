8184e2f7dcef6968fecc7e077e9299859b704335
Timothy Robert Sharp <timothyrobertsharp@crlMBP-L70JHXMGH9MzAw.local>
2025-05-22 16:01:24 -0700

    sadf

--- network.tf
+++ network.tf
@@ -53,7 +53,6 @@ resource "aws_route_table" "public_route_table" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-
 }
 
 resource "aws_route_table" "private_route_table" {
